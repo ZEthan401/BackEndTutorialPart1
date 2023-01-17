87a6156b1df3e36efffe1ca70ae8cecef38c2120
ZEthan401 <99200261+ZEthan401@users.noreply.github.com>
2023-01-17 13:58:01 -0500

    Add files via upload

---
+++ se.js
@@ -0,0 +1,17 @@
+const { write } = require('fs');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.write('Hello, World! ');
+        res.write('This is Web Dev!');
+        res.end();
+    }
+    if (req.url === '/api/courses'){
+        res.write(JSON.stringify([1, 2, 3]));
+        res.write('This is a list of offerings at BTHS.');
+        res.end();
+    }
+});
+
+console.log("Listening on port 3000...");
